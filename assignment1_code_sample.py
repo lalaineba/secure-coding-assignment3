a779e2a6d4dbdf68c178535287225aa0951c1e08
Lalaine Balmediano <lbalmediano3@academic.rrc.ca>
2025-10-27 12:53:34 -0500

    Update Python script

--- assignment1_code_sample.py
+++ assignment1_code_sample.py
@@ -5,7 +5,7 @@ from urllib.request import urlopen
 db_config = {
     'host': 'mydatabase.com',
     'user': 'admin',
-    'password': 'secret12'
+    'password': 'secret123'
 }
 
 def get_user_input():
